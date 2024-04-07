01845f4e5cc0f9e2c6da6ef9d8bb801245346c10
Cecilia Adika <ceciliaadika13@gmail.com>
2024-04-07 12:12:49 +0300

    Creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// Define findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Define fuzzyMatch function
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Define matchName function
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
